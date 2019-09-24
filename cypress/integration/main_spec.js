9584e16e382d13d919cc7ed9568effd3841f63a5
Miguel Puig <Miguel.PuigGarcia@ig.com>
2019-09-24 16:07:26 +0100

    changes

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -2,15 +2,15 @@
 
 describe('IG test', function(){
     it('Should have menus', function() {
-        cy.visit('https://lamaisonmobilecatering.com/')
+        cy.visit('/')
         cy.get('.menu-horizontal').contains('Menus').click()
         cy.url().should('include', 'menus')
         cy.contains('a', 'View menu')
     })
 
     it('Should have access to about us', function() {
-        cy.visit('https://lamaisonmobilecatering.com/')        
-        cy.get('.menu-horizontal').contains('About').click()
+        cy.visit('/')  
+        cy.get('.menu-horizontal').contains('About La Maison').click()
         cy.url().should('include', 'about')
         cy.get('h1').contains('About')
 
